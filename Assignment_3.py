bfdf6833a9d54674efcef2da7afc2a710f20da07
Martin Dahlgren <mdahlgrengadd@users.noreply.github.com>
2025-05-19 01:45:09 +0200

    Refactor neighbor visualization: Update pretty_print_neighbours function to include fitness values and improve output formatting.

--- Assignment_3.py
+++ Assignment_3.py
@@ -4,6 +4,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 import random
 
+from utils import pretty_print_neighbours
+
 # The dataset is uploaded
 f = open("Assignment 3 medical_dataset.DATA")
 dataset_X = []
@@ -89,10 +91,10 @@ def yourAlgorithm():
     # find the best solution in the neighborhood
     # Generate all neighbors by flipping one bit at a time
     neighbours = generate_neighbours(currentSolution)
-    pretty_print_neighbours(currentSolution, neighbours)
-
     neighboursFitness = [calculateFitness(
         neighbours[i]) for i in range(len(neighbours))]
+    pretty_print_neighbours(currentSolution, neighbours,
+                            fitness, neighboursFitness)
 
     while False:
         FITNESS_CALCULATIONS_COUNTER += 1
@@ -127,51 +129,6 @@ def generate_neighbours(currentSolution):
 
 # Function to pretty print the neighbours with colors.
 # Made by ChatGPT only for the better visualization of the neighbours
-
-
-def pretty_print_neighbours(currentSolution, neighbours):
-    print("Initial solution:     ", currentSolution,
-          f"Fitness: {calculateFitness(currentSolution)}")
-
-    # ANSI color codes for different colors
-    colors = [
-        "\033[91m",  # Red
-        "\033[92m",  # Green
-        "\033[93m",  # Yellow
-        "\033[94m",  # Blue
-        "\033[95m",  # Magenta
-        "\033[96m",  # Cyan
-    ]
-    reset_color = "\033[0m"  # Reset to default terminal color
-    green_color = "\033[92m"
-
-    # Calculate all neighbour fitnesses
-    fitnesses = [calculateFitness(neighbour) for neighbour in neighbours]
-    max_fitness = max(fitnesses)
-
-    for i, neighbour in enumerate(neighbours):
-        # Identify which bit was flipped
-        flipped_bit = [j for j in range(
-            len(currentSolution)) if currentSolution[j] != neighbour[j]][0]
-
-        # Use color for the flipped bit
-        color = colors[i % len(colors)]
-        colored_neighbour = []
-        for j, bit in enumerate(neighbour):
-            if j == flipped_bit:
-                colored_neighbour.append(f"{color}{bit}{reset_color}")
-            else:
-                colored_neighbour.append(str(bit))
-
-        fitness = fitnesses[i]
-        # Color the best fitness in green
-        if fitness == max_fitness:
-            fitness_str = f"{green_color}Fitness: {fitness}{reset_color}"
-        else:
-            fitness_str = f"Fitness: {fitness}"
-
-        print(
-            f"Neighbour {i:2d} (bit {flipped_bit:2d}): [{', '.join(colored_neighbour)}] {fitness_str}")
 
 
 bestSolution, bestSolutionFitness = yourAlgorithm()
